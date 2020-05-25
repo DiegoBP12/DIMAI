09e9c19397c95d755b184807140786cbe1fc0ee1
DiegoBP12 <diegobolpar@hotmail.com>
2020-05-25 15:24:05 -0500

    update requirements

--- dimai/settings.py
+++ dimai/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
+
 import os
-import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,9 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'portfolio.apps.PortfolioConfig',
-    'dj-database-url',
-    'gunicorn',
-    'whitenoise'
 ]
 
 MIDDLEWARE = [
@@ -90,8 +87,6 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
--- dimai/wsgi.py
+++ dimai/wsgi.py
@@ -12,5 +12,3 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dimai.settings')
 application = get_wsgi_application()
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
